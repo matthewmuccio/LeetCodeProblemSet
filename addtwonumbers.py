8479aa7609f499fd0932a8efeb4baebb5bba94fe
Matthew Muccio <me@matthewmuccio.com>
2018-11-17 23:36:26 -0800

    Adds code for Add Two Numbers (problem #2).

---
+++ addtwonumbers.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummyHead = ListNode(0)
+        p = l1
+        q = l2
+        curr = dummyHead
+        carry = 0
+        while p is not None or q is not None:
+            x = p.val if p is not None else 0
+            y = q.val if q is not None else 0
+            s = carry + x + y
+            carry = s / 10
+            curr.next = ListNode(s % 10)
+            curr = curr.next
+            if p is not None:
+                p = p.next
+            if q is not None:
+                q = q.next
+        if carry > 0:
+            curr.next = ListNode(carry)
+        return dummyHead.next
+
+
+if __name__ == "__main__":
+    s = Solution()
+    l1 = ListNode(2)
+    l1.next = ListNode(4)
+    l1.next.next = ListNode(3)
+    l2 = ListNode(5)
+    l2.next = ListNode(6)
+    l2.next.next = ListNode(4)
+    result = s.addTwoNumbers(l1, l2)
+    print(result)
