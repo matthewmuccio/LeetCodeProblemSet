404985e881c3495f240899923be310606601b052
Matthew Muccio <me@matthewmuccio.com>
2018-11-26 16:44:31 -0500

    Adds code for String to Integer (atoi) (problem #8).

--- stringtointeger.py
+++ stringtointeger.py
@@ -4,36 +4,36 @@
 class Solution(object):
     def myAtoi(self, str):
         """
-        :type s: str
-        :rtype: bool
+        :type str: str
+        :rtype: int
         """
-        min = -2147483648
-        max = 2147483647
+        mn = -2147483648
+        mx = 2147483647
         res = ""
         str_list = list(str.strip())
-        for index, char in enumerate(str_list):
-            if len(res) > 0 and not char.isdigit():
+        for i, s in enumerate(str_list):
+            if len(res) > 0 and not s.isdigit():
                 break
-            elif index == 1 and not char.isdigit():
+            elif i == 1 and not s.isdigit():
                 return 0
-            elif char == '+':
+            elif s == "+":
                 continue
-            elif char.isdigit() or char == '-':
-                res += char
+            elif s.isdigit() or s == "-":
+                res += s
             else:
                 return 0
-        if res == '-' or len(res) == 0:
+        if res == "-" or len(res) == 0:
             return 0
         res = int(res)
-        if res > max:
-            return max
-        elif res < min:
-            return min
+        if res > mx:
+            return mx
+        elif res < mn:
+            return mn
         return res
 
 
 if __name__ == "__main__":
     s = Solution()
-    str = "4193 with words"
+    str = "     -42"
     result = s.myAtoi(str)
     print(result)
