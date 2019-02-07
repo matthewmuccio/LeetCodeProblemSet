3e0cfdca1e77634b5be4147e5cd7393f1ad9129c
Matthew Muccio <me@matthewmuccio.com>
2019-02-07 13:46:49 -0500

    Adds code for N-Queens II (problem #52).

---
+++ nqueensii.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+
+class Solution(object):
+    def totalNQueens(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        self.res = 0
+        self.dfs([-1] * n, 0)
+        return self.res
+
+    def dfs(self, nums, index):
+        if index == len(nums):
+            self.res += 1
+            return
+        for i in range(len(nums)):
+            nums[index] = i
+            if self.valid(nums, index):
+                self.dfs(nums, index + 1)
+
+    def valid(self, nums, n):
+        for i in range(n):
+            if nums[i] == nums[n] or abs(nums[n] - nums[i]) == n - i:
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 4
+    result = s.totalNQueens(n)
+    print(result)
