5c716d90c9ed735a609b92b482be308b52147499
Matthew Muccio <me@matthewmuccio.com>
2019-04-23 17:05:20 -0400

    Adds code for Same Tree (problem #100).

---
+++ sametree.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return p is q
+
+
+if __name__ == "__main__":
+    s = Solution()
+    p = TreeNode(1)
+    p.left = TreeNode(2)
+    p.right = TreeNode(3)
+    q = TreeNode(1)
+    q.left = TreeNode(2)
+    q.right = TreeNode(3)
+    result = s.isSameTree(p, q)
+    print(result)
